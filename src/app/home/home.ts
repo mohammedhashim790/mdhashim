4c44bc6d64703e70f58e2ba9c957fcacafc7ad92
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-09-04 22:47:13 -0300

    feat: Portfolio Page

---
+++ src/app/home/home.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home', imports: [], templateUrl: './home.html', styleUrl: './home.css',
+})
+export class Home implements OnInit {
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+}
