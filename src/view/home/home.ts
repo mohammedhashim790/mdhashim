a926ea898773bfebba13e75a9933dc538785912a
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-09-06 15:32:07 -0300

    feat: Theme Customization and enhanced UI

--- src/view/home/home.ts
+++ src/view/home/home.ts
@@ -7,8 +7,11 @@ export class Home implements OnInit {
 
   public calendarLink: string = "https://calendar.app.google/WdPV8gysBeRiwU4H8";
 
+  public linkedIn: string = "https://www.linkedin.com/in/mdhashim/";
+  public github: string = "https://github.com/mohammedhashim790";
+  public medium: string = "https://medium.com/@mohammedhashim790";
+  protected readonly window = window;
+
   ngOnInit(): void {
   }
-
-  protected readonly window = window;
 }
