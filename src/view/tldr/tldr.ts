08c88d5b39e39563c98e6cc5cce8c59deb3bf632
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-09-08 17:26:39 -0300

    feat: Included Projects

--- src/view/tldr/tldr.ts
+++ src/view/tldr/tldr.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {WINDOW} from '../../bloc/directive/utils/window';
 
 @Component({
-  selector: 'app-tldr',
-  imports: [],
-  templateUrl: './tldr.html',
-  styleUrl: './tldr.css'
+  selector: 'app-tldr', imports: [], templateUrl: './tldr.html', styleUrl: './tldr.css'
 })
 export class TLDR {
+
+  public calendarLink: string = "https://calendar.app.google/WdPV8gysBeRiwU4H8";
+
+  public linkedIn: string = "https://www.linkedin.com/in/mdhashim/";
+  public github: string = "https://github.com/mohammedhashim790";
+  public medium: string = "https://medium.com/@mohammedhashim790";
+
+  protected readonly window = inject(WINDOW);
 
 }
